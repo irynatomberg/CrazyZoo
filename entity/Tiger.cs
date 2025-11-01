c1d6f2dc43f29637d889bf7250db3cbad5a94422
Yevhenii Tomberg <yevhenii@tomberg.com>
2025-11-01 18:14:01 +0200

    Added new animals (Elephant, Wolf, Tiger) and updated ZooViewModel for enum integration

---
+++ entity/Tiger.cs
@@ -0,0 +1,14 @@
+﻿using CrazyZoo.interfaces;
+
+namespace CrazyZoo.entity
+{
+    public class Tiger : Animal, ICrazyAction
+    {
+        public override string MakeSound() => "Roaaar! Tiiger näitab kihvu! 🐯";
+
+        public string ActCrazy()
+        {
+            return $"{Name} hüppas basseini ja hakkas ujuma nagu kass! 🐾";
+        }
+    }
+}
